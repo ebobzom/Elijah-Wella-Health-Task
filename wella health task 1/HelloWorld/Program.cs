d3773e194c5705838d2b22cacdcb9895a69731a7
Elijah <ebobzom@gmail.com>
2022-04-02 18:30:37 +0100

    Wella Health Week 1 and 2 Tasks

---
+++ wella health task 1/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Elijah Bobzom, I am in Backend Track of Wella Schools");
+            Console.WriteLine("I love working with data and using logic to manipulate them, thats why I want to learn Backend");
+        }
+    }
+}
---
+++ wella health task 1/Task-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            int age = 234;
+            string name = "Elijah Bobzom";
+            double myDoubleNum = 5.99;  // Floating point number
+            char myLetter = 'D';         // Character
+            bool myBool = true;
+            long myNum = 15000000000L;
+            float myFloat = 6.29F;
+            double myDouble = 19.99D;
+            DateTime dt = new DateTime();
+            Nullable<int> x = null;
+            dynamic MyDynamicVar = 1;
+
+            // Task 2
+            // Implicit Conversion
+            int i = 35;
+            float f = (int)i;
+
+            float z = 99.99F;
+            int convertedValue = (int)z; 
+
+            // Example: Explicit Conversion
+            int p = 100;
+            uint u = (uint) p;
+
+            Console.WriteLine(u);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myFloat);
+            Console.WriteLine(age);
+            Console.WriteLine(name);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(convertedValue);
+            Console.WriteLine(f);
+            
+        }
+    }
+}
